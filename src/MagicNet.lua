2a212a1c7ab2d910992a041de3611e6ed67f907d
MatheusBack-end <email.matheussilva2@gmail.com>
2023-11-23 14:42:07 -0300

    start.sh added

--- src/MagicNet.lua
+++ src/MagicNet.lua
@@ -1,12 +1,9 @@
-require('autoload')
-
-local conf = io.open("conf.yaml", "r"):read("*all")
-local parse = yaml.load(conf, { all = true })
+require('src/autoload')
 
 Log:info("server started!!")
 
-local ip = parse[1].host
-local port = tonumber(parse[1].port)
+local ip = "0.0.0.0"
+local port = 19132
 
 local thread_manager = ThreadsManager:new()
 local session_manager = SessionManager:new(UdpServerSocket:new(ip, port), thread_manager)
---
+++ start.sh
@@ -0,0 +1,1 @@
+clear && lua src/MagicNet.lua
